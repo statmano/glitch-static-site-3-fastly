28f43034b60788c9b4d8e27a40996fd61ada567e
Sue Smith <sue@benormal.info>
2025-05-21 10:16:24 +0100

    Create serve.sh

---
+++ _migrate/serve.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This script runs when you click the Share button!
+
+npm run build 
+npx --yes @fastly/compute-js-static-publish@latest --root-dir=./build --kv-store-name=my-site-content 
+cd compute-js 
+npm run dev:publish 
+npm run dev:start
