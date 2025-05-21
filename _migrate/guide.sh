2de4984ff19a55d3bf31f54b6c995472d0995420
Sue Smith <sue@benormal.info>
2025-05-21 20:07:42 +0100

    Update guide.sh

--- _migrate/guide.sh
+++ _migrate/guide.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This script runs when your project starts in the Codespace!
 
-printf "\nHi! Let's see if we can get your static Glitch site migrated to Fastly Compute.\n\nThis project will attempt to build and preview your site on startup.\n\nUse the 🔎 Split button at the bottom to see your website and code side by side.\n\nUse the 🧪 Serve button to test your Compute site in the codespace then grab a Fastly API key and hit 🚀 Publish when you're ready.\n\nYou'll find the steps in MIGRATE.md (right-click in the codespace and open it as a preview).\n\n"
+printf "\nHi! Let's see if we can get your static Glitch site migrated to Fastly Compute.\n\nThis project will attempt to build and preview your site on startup.\n\nUse the 🔎 Split button at the bottom to see your website and code side by side.\n\nUse the 🧪 Serve button to test your Compute site in the codespace then grab a Fastly API key and hit 🚀 Publish when you're ready.\n\nYou'll find the steps in MIGRATE.md (right-click the file in the codespace and open it as a preview).\n\n"
