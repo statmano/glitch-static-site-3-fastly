32358f6fbd9d4e160ad23d6ac38ef7be7f7dcb22
Sue Smith <sue@benormal.info>
2025-05-22 16:25:16 +0100

    Update serve.sh

--- _migrate/serve.sh
+++ _migrate/serve.sh
@@ -2,7 +2,7 @@
 # This script runs when you click the Share button!
 
 npm run build 
-npx --yes @fastly/compute-js-static-publish@latest --root-dir=./build --kv-store-name=my-site-content 
+npx --yes @fastly/compute-js-static-publish@latest --root-dir=./build --kv-store-name=website-content 
 cd compute-js 
 npm run dev:publish 
 npm run dev:start
